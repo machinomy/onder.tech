b0012e6c303f890126724d6911f24db6cb0c0e81
MainOlma <mainolma@gmail.com>
2019-02-18 12:44:58 +0400

    minor improvements

--- assets/js/scroller.js
+++ assets/js/scroller.js
@@ -163,7 +163,7 @@ function changeScheme(j) {
     var clones = []
     if (!d3.select("path#clone0").node()) { //create 'ghost' lines if they are not exist yet
         for (var i = 0; i < 4; i++) {
-            clones.push(d3.select("path#p"+(j+1)).clone().attr("id", "clone" + i).attr("stroke-width", 4-i*0.5))
+            clones.push(d3.select("path#p"+(j+1)).clone().attr("id", "clone" + i).attr("stroke-width", 4-i))
         }
     }
     else {
